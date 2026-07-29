1f3dd0c1d5bfb4716ac3bc66415a770a50a032b2
YvonneGraceB <202580172@psu.palawan.edu.ph>
2026-07-30 01:36:59 +0800

    Create Practice01.3.cs

---
+++ Practice01.3.cs
@@ -0,0 +1,20 @@
+ Console.Write("Enter your age: ");
+ int age = int.Parse(Console.ReadLine());
+ Console.Write("Enter your monthly income: ");
+ double income = double.Parse(Console.ReadLine());
+ 
+ if (age >= 25 && age <= 35 && income > 50000)
+  {
+      Console.WriteLine("Young Professional");
+  }
+  else if (age >= 36 && age <= 55 && income > 70000)
+  {
+      Console.WriteLine("Middle-Aged Professional");
+  }
+  else if (age >= 56 && income < 30000)
+  {
+      Console.WriteLine("Senior Citizen");
+  }
+  else
+  {
+       Console.WriteLine("Other");
