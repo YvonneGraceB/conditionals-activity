d8454056d6964747179ac6e5766c32aa879258cf
YvonneGraceB <202580172@psu.palawan.edu.ph>
2026-07-30 01:31:41 +0800

    Update Practice01.4.cs

--- Practice01.4.cs
+++ Practice01.4.cs
@@ -1,5 +1,5 @@
 Console.Write("Enter your grade:");
-int number = int.Parse(Console.ReadLine());
+int grade = int.Parse(Console.ReadLine());
 
-string result = (number >= 50) ? "Passed": "Failed"
+string result = (grade >= 50) ? "Passed": "Failed"
 Console.WriteLine(result);
