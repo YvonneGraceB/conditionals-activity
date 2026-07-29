6e55a658d15c6569fcb0e559ee5eb6ba56e37e6b
YvonneGraceB <202580172@psu.palawan.edu.ph>
2026-07-30 01:15:52 +0800

    Update and rename Practice00.2.cs to Practice01.2.cs

---
+++ Practice01.2.cs
@@ -0,0 +1,10 @@
+Console.Write("Enter any number:");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+  Console.WriteLine("The number [number] is even.");
+}
+else
+{
+  Console.WriteLine("The number [number is negative."):
